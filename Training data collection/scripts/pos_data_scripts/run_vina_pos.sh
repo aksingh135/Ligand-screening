582ec0881b3aeae29f8775bba89b2982d05e983e
Singh, Akshita <asingh970@gatech.edu>
2024-01-21 17:34:46 -0800

    Create run_vina_pos.sh

---
+++ Training data collection/scripts/pos_data_scripts/run_vina_pos.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+for f in n_ligand*.pdbqt; do
+    b=`basename $f .pdbqt`
+    echo Processing ligand $b
+    mkdir -p $b
+    ./autodock_vina_1_1_2_linux_x86/bin/vina --config conf_all_p.txt --ligand $f --out ${b}/out.pdbqt --log ${b}/p_log.txt
+   
+done
