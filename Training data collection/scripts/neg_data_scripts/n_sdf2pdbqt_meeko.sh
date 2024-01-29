0688bf940b419182bb47160583091578b96e9962
aksingh135 <106146795+aksingh135@users.noreply.github.com>
2024-01-29 14:00:02 -0500

    Delete Training data collection/scripts/neg_data_scripts directory

--- Training data collection/scripts/neg_data_scripts/n_sdf2pdbqt_meeko.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in cid_*.sdf; do
-  basename="${file%.sdf}"
-  echo "Coverting $file to ${basename}.pdbqt"
-  mk_prepare_ligand.py -i "$file" -o "${basename}.pdbqt"
-
-done
-  
--- Training data collection/scripts/neg_data_scripts/run_vina_neg.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-for f in n_ligand*.pdbqt; do
-    b=`basename $f .pdbqt`
-    echo Processing ligand $b
-    mkdir -p $b
-    ./autodock_vina_1_1_2_linux_x86/bin/vina --config conf_all_n.txt --ligand $f --out ${b}/out.pdbqt --log ${b}/n_log.txt
-   
-done
